f8c9b4dcc53db0acc87bb8327c932514ec9b5cb4
Gibran Dar <gibran.dar@wearemapp.com>
2023-08-11 15:26:04 +0100

    add query for specific property

--- src/hubspotapilib/properties.py
+++ src/hubspotapilib/properties.py
@@ -1,9 +1,16 @@
+from typing import Union
+
 from . import hubspot_client
 from .schemas import AssociatedHsObject, HsPropertyResults
 
 
-def real_all_properties(object_type: AssociatedHsObject, archived: bool = False) -> HsPropertyResults:
+def real_all_properties(
+    object_type: AssociatedHsObject, property: str = "", archived: bool = False
+) -> HsPropertyResults:
     with hubspot_client() as client:
-        res = client.crm.properties.core_api.get_all(object_type, archived=archived)
+        request: dict[str, Union[str, bool]] = {"object_type": object_type, "archived": archived}
+        if property:
+            request["property"] = property
+        res = client.crm.properties.core_api.get_all(**request)
         properties: HsPropertyResults = res.to_dict()
         return properties
